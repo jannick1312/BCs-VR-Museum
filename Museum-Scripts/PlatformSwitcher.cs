cd11c88030b9b46c28205f343705b50d0a24f618
Jannick <j.seper@stud.unibas.ch>
2026-05-15 11:37:32 +0200

    Cleanup and makes environment change possible

--- Museum-Scripts/PlatformSwitcher.cs
+++ Museum-Scripts/PlatformSwitcher.cs
@@ -4,7 +4,14 @@ public partial class PlatformSwitcher : Node
 {
 	[Export] public NodePath PlayerRigPath;
 	[Export] public NodePath CameraPath;
+
+	[Export] public NodePath MuseumNodePath;
+	[Export] public NodePath MenuNodePath;
 	[Export] public NodePath MenuSpawnPointPath;
+
+	[Export] public NodePath WorldEnvironmentPath;
+	[Export] public Environment MuseumEnvironment;
+	[Export] public Environment MenuEnvironment;
 
 	[Export] public NodePath LeftMovementPath;
 	[Export] public NodePath RightTurnMovementPath;
@@ -12,27 +19,41 @@ public partial class PlatformSwitcher : Node
 
 	private Node3D _playerRig;
 	private XRCamera3D _camera;
+
+	private Node3D _museumNode;
+	private Node3D _menuNode;
 	private Marker3D _menuSpawnPoint;
+
+	private WorldEnvironment _worldEnvironment;
 
 	private Node _leftMovement;
 	private Node _rightTurnMovement;
 	private Node _rightJumpMovement;
 
-	private Transform3D _lastMainTransform;
+	private Transform3D _lastMuseumTransform;
 
-	private bool _isOnMenuPlatform = false;
+	private bool _isInMenu = false;
 	private bool _bothWerePressed = false;
 
 	public override void _Ready()
 	{
 		_playerRig = GetNode<Node3D>(PlayerRigPath);
 		_camera = GetNode<XRCamera3D>(CameraPath);
+
+		_museumNode = GetNode<Node3D>(MuseumNodePath);
+		_menuNode = GetNode<Node3D>(MenuNodePath);
 		_menuSpawnPoint = GetNode<Marker3D>(MenuSpawnPointPath);
+
+		_worldEnvironment = GetNode<WorldEnvironment>(WorldEnvironmentPath);
 
 		_leftMovement = GetNode<Node>(LeftMovementPath);
 		_rightTurnMovement = GetNode<Node>(RightTurnMovementPath);
 		_rightJumpMovement = GetNode<Node>(RightJumpMovementPath);
 
+		_worldEnvironment.Environment = MuseumEnvironment;
+
+		SetMuseumActive(true);
+		SetMenuActive(false);
 		SetMovementEnabled(true);
 	}
 
@@ -41,36 +62,50 @@ public partial class PlatformSwitcher : Node
 		XRController3D left = GetTree().Root.FindChild("LeftController", true, false) as XRController3D;
 		XRController3D right = GetTree().Root.FindChild("RightController", true, false) as XRController3D;
 
+		if (left == null || right == null)
+			return;
+
 		bool bothPressed =
 			left.GetFloat("trigger") > 0.75f &&
 			right.GetFloat("trigger") > 0.75f;
 
 		if (bothPressed && !_bothWerePressed)
-			TogglePlatform();
+			ToggleWorld();
 
 		_bothWerePressed = bothPressed;
 
-		if (_isOnMenuPlatform)
+		if (_isInMenu)
 			LockCameraToMenuSpawn();
 	}
 
-	private void TogglePlatform()
+	private void ToggleWorld()
 	{
-		if (_isOnMenuPlatform)
+		if (_isInMenu)
 		{
-			_playerRig.GlobalTransform = _lastMainTransform;
+			SetMenuActive(false);
+			SetMuseumActive(true);
+
+			_worldEnvironment.Environment = MuseumEnvironment;
+
+			_playerRig.GlobalTransform = _lastMuseumTransform;
+
 			SetMovementEnabled(true);
-			_isOnMenuPlatform = false;
+			_isInMenu = false;
 		}
 		else
 		{
-			_lastMainTransform = _playerRig.GlobalTransform;
+			_lastMuseumTransform = _playerRig.GlobalTransform;
+
+			SetMuseumActive(false);
+			SetMenuActive(true);
+
+			_worldEnvironment.Environment = MenuEnvironment;
 
 			_playerRig.GlobalTransform = _menuSpawnPoint.GlobalTransform;
 			LockCameraToMenuSpawn();
 
 			SetMovementEnabled(false);
-			_isOnMenuPlatform = true;
+			_isInMenu = true;
 		}
 	}
 
@@ -78,6 +113,18 @@ public partial class PlatformSwitcher : Node
 	{
 		Vector3 difference = _menuSpawnPoint.GlobalPosition - _camera.GlobalPosition;
 		_playerRig.GlobalPosition += difference;
+	}
+
+	private void SetMuseumActive(bool active)
+	{
+		_museumNode.Visible = active;
+		_museumNode.ProcessMode = active ? ProcessModeEnum.Inherit : ProcessModeEnum.Disabled;
+	}
+
+	private void SetMenuActive(bool active)
+	{
+		_menuNode.Visible = active;
+		_menuNode.ProcessMode = active ? ProcessModeEnum.Inherit : ProcessModeEnum.Disabled;
 	}
 
 	private void SetMovementEnabled(bool enabled)
