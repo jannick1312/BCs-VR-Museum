523a2cbec6cdc797d6651fb16bb5cb0051507b53
Jannick <j.seper@stud.unibas.ch>
2026-05-17 15:52:59 +0200

    Solves problem of falling player in MenuNode

--- PlatformSwitcher.cs
+++ PlatformSwitcher.cs
@@ -2,96 +2,97 @@ using Godot;
 
 public partial class PlatformSwitcher : Node
 {
-	[Export] public NodePath PlayerRigPath;
-	[Export] public NodePath CameraPath;
-
+	[Export] public NodePath PlayerPath;
 	[Export] public NodePath MuseumNodePath;
 	[Export] public NodePath MenuNodePath;
-	[Export] public NodePath MenuSpawnPointPath;
-
 	[Export] public NodePath WorldEnvironmentPath;
 	[Export] public Environment MuseumEnvironment;
 	[Export] public Environment MenuEnvironment;
 
-	[Export] public NodePath LeftMovementPath;
-	[Export] public NodePath RightTurnMovementPath;
-	[Export] public NodePath RightJumpMovementPath;
-
-	[Export] public NodePath LeftHandVisualPath;
-	[Export] public NodePath RightHandVisualPath;
-
+	private Node _player;
 	private Node3D _playerRig;
 	private XRCamera3D _camera;
-
+	private XRController3D _leftController;
+	private XRController3D _rightController;
+	private Node3D _leftHandVisual;
+	private Node3D _rightHandVisual;
 	private Node3D _museumNode;
 	private Node3D _menuNode;
 	private Marker3D _menuSpawnPoint;
-
 	private WorldEnvironment _worldEnvironment;
-
 	private Node _leftMovement;
 	private Node _rightTurnMovement;
 	private Node _rightJumpMovement;
-
-	private Node3D _leftHandVisual;
-	private Node3D _rightHandVisual;
-
+	
 	private Transform3D _lastMuseumTransform;
-
+	private Transform3D _lockedMenuTransform;
 	private bool _isInMenu = false;
 	private bool _bothWerePressed = false;
 
 	public override void _Ready()
 	{
-		_playerRig = GetNode<Node3D>(PlayerRigPath);
-		_camera = GetNode<XRCamera3D>(CameraPath);
+		_player = GetNode(PlayerPath);
+
+		_playerRig = _player.FindChild("XROrigin3D", true, false) as Node3D;
+		_camera = _player.FindChild("XRCamera3D", true, false) as XRCamera3D;
+
+		_leftController = _player.FindChild("LeftController", true, false) as XRController3D;
+		_rightController = _player.FindChild("RightController", true, false) as XRController3D;
+
+		_leftHandVisual = _player.FindChild("LeftHand2", true, false) as Node3D;
+		_rightHandVisual = _player.FindChild("RightHand2", true, false) as Node3D;
+
+		_leftMovement = _player.FindChild("MovementDirect", true, false);
+		_rightTurnMovement = _player.FindChild("MovementTurn", true, false);
+		_rightJumpMovement = _player.FindChild("MovementJump", true, false);
 
 		_museumNode = GetNode<Node3D>(MuseumNodePath);
 		_menuNode = GetNode<Node3D>(MenuNodePath);
-		_menuSpawnPoint = GetNode<Marker3D>(MenuSpawnPointPath);
+
+		_menuSpawnPoint = _menuNode.FindChild("MenuSpawnPoint", true, false) as Marker3D;
 
 		_worldEnvironment = GetNode<WorldEnvironment>(WorldEnvironmentPath);
-
-		_leftMovement = GetNode<Node>(LeftMovementPath);
-		_rightTurnMovement = GetNode<Node>(RightTurnMovementPath);
-		_rightJumpMovement = GetNode<Node>(RightJumpMovementPath);
-
-		_leftHandVisual = GetNode<Node3D>(LeftHandVisualPath);
-		_rightHandVisual = GetNode<Node3D>(RightHandVisualPath);
 
 		_worldEnvironment.Environment = MuseumEnvironment;
 
 		SetMuseumActive(true);
 		SetMenuActive(false);
 		SetMovementEnabled(true);
-		SetHandVisualsVisible(true);
 	}
 
 	public override void _Process(double delta)
 	{
-		XRController3D left = GetTree().Root.FindChild("LeftController", true, false) as XRController3D;
-		XRController3D right = GetTree().Root.FindChild("RightController", true, false) as XRController3D;
-
-		if (left == null || right == null)
-			return;
 
 		bool bothPressed =
-			left.GetFloat("trigger") > 0.75f &&
-			right.GetFloat("trigger") > 0.75f;
+			_leftController.GetFloat("trigger") > 0.75f &&
+			_rightController.GetFloat("trigger") > 0.75f;
 
 		if (bothPressed && !_bothWerePressed)
 			ToggleWorld();
 
 		_bothWerePressed = bothPressed;
+	}
 
-		if (_isInMenu)
-			LockCameraToMenuSpawn();
+	public override void _PhysicsProcess(double delta)
+	{
+		if (!_isInMenu)
+			return;
+
+		_playerRig.GlobalTransform = _lockedMenuTransform;
+
+		if (_leftHandVisual != null && _leftController != null)
+			_leftHandVisual.GlobalTransform = _leftController.GlobalTransform;
+
+		if (_rightHandVisual != null && _rightController != null)
+			_rightHandVisual.GlobalTransform = _rightController.GlobalTransform;
 	}
 
 	private void ToggleWorld()
 	{
 		if (_isInMenu)
 		{
+			_isInMenu = false;
+
 			SetMenuActive(false);
 			SetMuseumActive(true);
 
@@ -100,33 +101,34 @@ public partial class PlatformSwitcher : Node
 			_playerRig.GlobalTransform = _lastMuseumTransform;
 
 			SetMovementEnabled(true);
-			SetHandVisualsVisible(true);
-
-			_isInMenu = false;
 		}
 		else
 		{
 			_lastMuseumTransform = _playerRig.GlobalTransform;
+
+			SetMovementEnabled(false);
 
 			SetMuseumActive(false);
 			SetMenuActive(true);
 
 			_worldEnvironment.Environment = MenuEnvironment;
 
-			_playerRig.GlobalTransform = _menuSpawnPoint.GlobalTransform;
-			LockCameraToMenuSpawn();
+			MoveCameraExactlyToMenuSpawn();
 
-			SetMovementEnabled(false);
-			SetHandVisualsVisible(false);
+			_lockedMenuTransform = _playerRig.GlobalTransform;
 
 			_isInMenu = true;
 		}
 	}
 
-	private void LockCameraToMenuSpawn()
+	private void MoveCameraExactlyToMenuSpawn()
 	{
-		Vector3 difference = _menuSpawnPoint.GlobalPosition - _camera.GlobalPosition;
-		_playerRig.GlobalPosition += difference;
+		if (_menuSpawnPoint == null)
+			return;
+
+		Vector3 cameraOffset = _camera.GlobalPosition - _playerRig.GlobalPosition;
+
+		_playerRig.GlobalPosition = _menuSpawnPoint.GlobalPosition - cameraOffset;
 
 		Vector3 rotation = _playerRig.GlobalRotation;
 		rotation.Y = _menuSpawnPoint.GlobalRotation.Y;
@@ -145,14 +147,17 @@ public partial class PlatformSwitcher : Node
 
 	private void SetMovementEnabled(bool enabled)
 	{
-		_leftMovement.Set("enabled", enabled);
-		_rightTurnMovement.Set("enabled", enabled);
-		_rightJumpMovement.Set("enabled", enabled);
+		SetNodeEnabled(_leftMovement, enabled);
+		SetNodeEnabled(_rightTurnMovement, enabled);
+		SetNodeEnabled(_rightJumpMovement, enabled);
 	}
 
-	private void SetHandVisualsVisible(bool visible)
+	private void SetNodeEnabled(Node node, bool enabled)
 	{
-		_leftHandVisual.Visible = visible;
-		_rightHandVisual.Visible = visible;
+		node.Set("enabled", enabled);
+
+		node.ProcessMode = enabled
+			? Node.ProcessModeEnum.Inherit
+			: Node.ProcessModeEnum.Disabled;
 	}
 }
