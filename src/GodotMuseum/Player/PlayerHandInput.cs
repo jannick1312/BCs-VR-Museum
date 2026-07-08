ed89e0061ad02645fd8a532e5870a1f89410f729
Jannick Seper <j.seper@stud.unibas.ch>
2026-07-08 17:28:35 +0200

    Remove unnecessary streaming input forcing

--- src/GodotMuseum/Player/PlayerHandInput.cs
+++ src/GodotMuseum/Player/PlayerHandInput.cs
@@ -6,27 +6,22 @@ public partial class PlayerHandInput : Node
 {
 	[Export] public StringName PinchAction { get; set; }
 	[Export] public StringName GripAction { get; set; }
-	[Export] public StringName MovementAction { get; set; }
 	[Export] public float PinchThreshold { get; set; }
 	[Export] public float PinchReleaseThreshold { get; set; }
 	[Export] public float GrabThreshold { get; set; }
 	[Export] public float GrabReleaseThreshold { get; set; }
 	[Export] public float LeftPinchMoveDelaySeconds { get; set; }
-	[Export] public float ControllerInputActiveThreshold { get; set; }
 
 	private Node3D _player;
 	private Node _playerBody;
 	private Node3D _leftTrackedHand;
 	private Node3D _rightTrackedHand;
-	private XRController3D _leftController;
-	private XRController3D _rightController;
 	private HandJoystickMovement _leftMovement;
 	private PlayerInputModeDetector _modeDetector;
 	private PlayerInputVisuals _visuals;
 	private HandGestureInput _gestures;
 	private bool _leftHandTrackingActive;
 	private bool _rightHandTrackingActive;
-	private PlayerInputMode? _activeVisualMode;
 
 	public override void _Ready()
 	{
@@ -34,8 +29,6 @@ public partial class PlayerHandInput : Node
 		_playerBody = _player.FindChild("PlayerBody", true, false);
 		_leftTrackedHand = (Node3D)_player.FindChild("LeftTrackedHand", true, false);
 		_rightTrackedHand = (Node3D)_player.FindChild("RightTrackedHand", true, false);
-		_leftController = (XRController3D)_player.FindChild("LeftController", true, false);
-		_rightController = (XRController3D)_player.FindChild("RightController", true, false);
 		_leftMovement = (HandJoystickMovement)_player.FindChild("Movement", true, false);
 		_leftMovement.Configure(_player);
 
@@ -62,16 +55,9 @@ public partial class PlayerHandInput : Node
 
 	public override void _Process(double delta)
 	{
-		var profileMode = _modeDetector.GetMode();
-		var controllerProfileMode = profileMode == PlayerInputMode.Controller;
-		var leftTrackerActive = IsHandTrackerActive(_leftTrackedHand);
-		var rightTrackerActive = IsHandTrackerActive(_rightTrackedHand);
-		var anyHandTrackerActive = leftTrackerActive || rightTrackerActive;
-		var anyControllerTrackerActive = IsControllerTrackerActive(_leftController) || IsControllerTrackerActive(_rightController);
-		UpdateActiveVisualMode(controllerProfileMode, anyHandTrackerActive, anyControllerTrackerActive);
-		var controllerMode = _activeVisualMode == PlayerInputMode.Controller;
-		_leftHandTrackingActive = !controllerMode && leftTrackerActive;
-		_rightHandTrackingActive = !controllerMode && rightTrackerActive;
+		var controllerMode = _modeDetector.GetMode() == PlayerInputMode.Controller;
+		_leftHandTrackingActive = !controllerMode && IsHandTrackerActive(_leftTrackedHand);
+		_rightHandTrackingActive = !controllerMode && IsHandTrackerActive(_rightTrackedHand);
 		_gestures.SetRightPointerNativeActionEnabled(!_rightHandTrackingActive);
 		_visuals.Apply(
 			controllerMode,
@@ -102,57 +88,5 @@ public partial class PlayerHandInput : Node
 	private static bool IsHandTrackerActive(Node3D trackedHand)
 	{
 		return ((XRNode3D)trackedHand).GetIsActive();
-	}
-
-	private static bool IsControllerTrackerActive(XRController3D controller)
-	{
-		return controller.GetIsActive();
-	}
-
-	private void UpdateActiveVisualMode(bool controllerProfileMode, bool anyHandTrackerActive, bool anyControllerTrackerActive)
-	{
-		var controllerInputActive = controllerProfileMode && IsControllerInputActive();
-
-		if (controllerInputActive)
-		{
-			_activeVisualMode = PlayerInputMode.Controller;
-			return;
-		}
-
-		if (_activeVisualMode == PlayerInputMode.Controller)
-		{
-			if (!anyControllerTrackerActive && anyHandTrackerActive)
-				_activeVisualMode = PlayerInputMode.Hand;
-
-			return;
-		}
-
-		if (_activeVisualMode == PlayerInputMode.Hand)
-		{
-			if (!anyHandTrackerActive && anyControllerTrackerActive && controllerProfileMode)
-				_activeVisualMode = PlayerInputMode.Controller;
-
-			return;
-		}
-
-		_activeVisualMode = controllerProfileMode && anyControllerTrackerActive && !anyHandTrackerActive
-			? PlayerInputMode.Controller
-			: PlayerInputMode.Hand;
-	}
-
-	private bool IsControllerInputActive()
-	{
-		return IsControllerInputActive(_leftController) || IsControllerInputActive(_rightController);
-	}
-
-	private bool IsControllerInputActive(XRController3D controller)
-	{
-		var movement = controller.GetVector2(MovementAction);
-		return movement.Length() > ControllerInputActiveThreshold ||
-			controller.IsButtonPressed("primary_click") ||
-			controller.IsButtonPressed("secondary_click") ||
-			controller.IsButtonPressed("ax_button") ||
-			controller.IsButtonPressed("by_button") ||
-			controller.IsButtonPressed("menu_button");
 	}
 }
